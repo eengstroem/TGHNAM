6b8754d0455a0ded805c4f6d8b455081f976d9d0
eengstroem <201705234@post.au.dk>
2022-09-27 13:29:24 +0200

    player health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
+    public int maxHealth;
+    public int currentHealth;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    void TakeDamage(int amount)
     {
-        
+        currentHealth = currentHealth-1;
     }
 }
