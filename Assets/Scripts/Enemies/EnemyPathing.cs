dfe7b7b88eea6bdf195f7009954b34077256b71f
Marius Hambro Bovbjerg <marius@hambro.eu>
2022-10-11 10:10:07 +0200

    movement

--- Assets/Scripts/Enemies/EnemyPathing.cs
+++ Assets/Scripts/Enemies/EnemyPathing.cs
@@ -5,13 +5,12 @@ namespace Assets.Scripts.Enemies
     public class EnemyPathing : MonoBehaviour
     {
         private GameObject player;
-        public float movementSpeed = 0.5F;
-        public CapsuleCollider2D m_Collider;
+        public float movementSpeed = 5f;
+        public Rigidbody2D rb;
 
         void Awake()
         {
             player = GameObject.FindWithTag("Player");
-            Debug.Log(player);
         }
 
         // Start is called before the first frame update
@@ -22,8 +21,9 @@ namespace Assets.Scripts.Enemies
         // Update is called once per frame
         void Update()
         {
-            float step = movementSpeed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
+            var step = movementSpeed * Time.deltaTime;
+            var direction = (player.transform.position - transform.position).normalized;
+            rb.MovePosition(transform.position + direction * step);
         }
     }
 }
