300734efb920659587e9749472335158cb4075ec
eengstroem <201705234@post.au.dk>
2022-10-11 09:18:12 +0200

    funny

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,26 +1,26 @@
+using System;
 using UnityEngine;
 
-namespace Assets.Scripts.Player
+public class PlayerHealth : MonoBehaviour
 {
-    public class PlayerHealth : MonoBehaviour
-    {
-        public float maxHealth;
-        public float currentHealth;
+    public static event Action OnPlayerDeath;
+    public float maxHealth;
+    public float currentHealth;
     
-        // Start is called before the first frame update
-        void Start()
-        {
-            currentHealth = maxHealth;
-        }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
 
-        public void TakeDamage(int amount)
-        {
-            currentHealth = currentHealth-amount;
+    public void TakeDamage(int amount)
+    {
+        currentHealth = currentHealth-amount;
         
-            if (currentHealth <= 0)
-            {
-                //Become dead
-            }
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            OnPlayerDeath?.Invoke();
         }
     }
 }
---
+++ Assets/Scripts/UI/RetryMenu.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryMenu : MonoBehaviour
+{
+    public GameObject gameOverMenu;
+
+    private void OnEnable()
+    {
+        PlayerHealth.OnPlayerDeath += EnableGameOverScreen;
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealth.OnPlayerDeath -= EnableGameOverScreen;
+    }
+
+    public void EnableGameOverScreen()
+    {
+        gameOverMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+}
