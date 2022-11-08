9ddecce32fba06a2018200bd2e29602d3689994a
eengstroem <201705234@post.au.dk>
2022-11-08 12:32:56 +0100

    added new camera stuff, and biiiig level

--- Assets/Scripts/Camera/CameraBehaviour.cs
+++ Assets/Scripts/Camera/CameraBehaviour.cs
@@ -6,16 +6,39 @@ namespace Assets.Scripts.Camera
     public class CameraBehaviour : MonoBehaviour
     {
         public PlayerMovement player;
-
+        private float xMin, xMax, yMin, yMax;
+        private float camX, camY;
+        private float camOrthSize;
+        private float screenAspect;
+        private float cameraRatio;
+        private UnityEngine.Camera mainCamera;
+        
+        public Transform followTransform;
+        public EdgeCollider2D mapBounds;
         // Start is called before the first frame update
         void Start()
         {
+            xMin = mapBounds.bounds.min.x;
+            yMin = mapBounds.bounds.min.y;
+            xMax = mapBounds.bounds.max.x;
+            yMax = mapBounds.bounds.max.y;
+            mainCamera = GetComponent<UnityEngine.Camera>();
+            camOrthSize = mainCamera.orthographicSize;
+            cameraRatio = (xMax + camOrthSize) / 2.0f;
         }
-
-        // Update is called once per frame
-        void Update()
+        
+        // // Update is called once per frame
+        // void Update()
+        // {
+        //     transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        // }
+        
+        private void FixedUpdate()
         {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+            camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthSize, yMax - camOrthSize);
+            camX = Mathf.Clamp(followTransform.position.x, xMin + camOrthSize, xMax - camOrthSize);
+            this.transform.position = new Vector3(camX, camY,
+                this.transform.position.z);
         }
     }
 }
